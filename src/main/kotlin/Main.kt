abb539eeef6385698c500959e36e54acd2077a58
queen-bellah <warukiralynette@gmail.com>
2024-02-08 20:09:16 +0300

    i added a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var fullName="Queen Bella"
+    var age=22
+    var phoneNumber="0780768445"
+    var kg=68.7F
+    var kenyan=false
+    println(fullName)
+println(age)
+    println(phoneNumber)
+    println(kg)
+    println(kenyan)
+
+
+}
